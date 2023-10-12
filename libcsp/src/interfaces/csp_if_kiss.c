3017cc01df56f7d37d85514c3d83d2aa9bb00dd0
Colin Waddell <cwaddell@benchmark-space.com>
2023-10-12 13:31:11 +0100

    Tidy up of working code

--- libcsp/src/interfaces/csp_if_kiss.c
+++ libcsp/src/interfaces/csp_if_kiss.c
@@ -22,224 +22,214 @@ Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA  02110-1301  USA
 
 #include <string.h>
 
-#include <csp/csp_endian.h>
 #include <csp/csp_crc32.h>
+#include <csp/csp_endian.h>
 
-#define FEND  		0xC0
-#define FESC  		0xDB
-#define TFEND 		0xDC
-#define TFESC 		0xDD
-#define TNC_DATA	0x00
+#define FEND 0xC0
+#define FESC 0xDB
+#define TFEND 0xDC
+#define TFESC 0xDD
+#define TNC_DATA 0x00
 
-int csp_kiss_tx(const csp_route_t * ifroute, csp_packet_t * packet) {
+int csp_kiss_tx(const csp_route_t *ifroute, csp_packet_t *packet) {
+    csp_kiss_interface_data_t *ifdata = ifroute->iface->interface_data;
+    void *driver = ifroute->iface->driver_data;
 
-	csp_kiss_interface_data_t * ifdata = ifroute->iface->interface_data;
-	void * driver = ifroute->iface->driver_data;
+    /* Add CRC32 checksum - the MTU setting ensures there are space */
+    csp_crc32_append(packet, false);
 
-	/* Add CRC32 checksum - the MTU setting ensures there are space */
-	csp_crc32_append(packet, false);
+    /* Lock */
+    if (csp_mutex_lock(&ifdata->lock, 1000) != CSP_MUTEX_OK) {
+        return CSP_ERR_TIMEDOUT;
+    }
 
-	/* Lock */
-	if (csp_mutex_lock(&ifdata->lock, 1000) != CSP_MUTEX_OK) {
-            return CSP_ERR_TIMEDOUT;
+    /* Save the outgoing id in the buffer */
+    packet->id.ext = csp_hton32(packet->id.ext);
+    packet->length += sizeof(packet->id.ext);
+
+    /* Transmit data */
+    const unsigned char start[] = { FEND, TNC_DATA };
+    const unsigned char esc_end[] = { FESC, TFEND };
+    const unsigned char esc_esc[] = { FESC, TFESC };
+    const unsigned char *data = (unsigned char *)&packet->id.ext;
+    ifdata->tx_func(driver, start, sizeof(start));
+    for (unsigned int i = 0; i < packet->length; i++, ++data) {
+        if (*data == FEND) {
+            ifdata->tx_func(driver, esc_end, sizeof(esc_end));
+            continue;
         }
+        if (*data == FESC) {
+            ifdata->tx_func(driver, esc_esc, sizeof(esc_esc));
+            continue;
+        }
+        ifdata->tx_func(driver, data, 1);
+    }
+    const unsigned char stop[] = { FEND };
+    ifdata->tx_func(driver, stop, sizeof(stop));
 
-	/* Save the outgoing id in the buffer */
-	packet->id.ext = csp_hton32(packet->id.ext);
-	packet->length += sizeof(packet->id.ext);
+    /* Free data */
+    csp_buffer_free(packet);
 
-	/* Transmit data */
-        const unsigned char start[] = {FEND, TNC_DATA};
-        const unsigned char esc_end[] = {FESC, TFEND};
-        const unsigned char esc_esc[] = {FESC, TFESC};
-        const unsigned char * data = (unsigned char *) &packet->id.ext;
-        ifdata->tx_func(driver, start, sizeof(start));
-	for (unsigned int i = 0; i < packet->length; i++, ++data) {
-		if (*data == FEND) {
-                    ifdata->tx_func(driver, esc_end, sizeof(esc_end));
-                    continue;
-		}
-                if (*data == FESC) {
-                    ifdata->tx_func(driver, esc_esc, sizeof(esc_esc));
-                    continue;
-		}
-		ifdata->tx_func(driver, data, 1);
-	}
-        const unsigned char stop[] = {FEND};
-        ifdata->tx_func(driver, stop, sizeof(stop));
+    /* Unlock */
+    csp_mutex_unlock(&ifdata->lock);
 
-	/* Free data */
-	csp_buffer_free(packet);
-
-	/* Unlock */
-	csp_mutex_unlock(&ifdata->lock);
-
-	return CSP_ERR_NONE;
+    return CSP_ERR_NONE;
 }
 
 /**
  * Decode received data and eventually route the packet.
  */
-void csp_kiss_rx(csp_iface_t * iface, const uint8_t * buf, size_t len, void * pxTaskWoken) {
+void csp_kiss_rx(csp_iface_t *iface, const uint8_t *buf, size_t len, void *pxTaskWoken) {
+    csp_kiss_interface_data_t *ifdata = iface->interface_data;
 
-	csp_kiss_interface_data_t * ifdata = iface->interface_data;
+    while (len--) {
+        /* Input */
+        uint8_t inputbyte = *buf++;
 
-	while (len--) {
+        /* If packet was too long */
+        if (ifdata->rx_length > ifdata->max_rx_length) {
+            //csp_log_warn("KISS RX overflow");
+            iface->rx_error++;
+            ifdata->rx_mode = KISS_MODE_NOT_STARTED;
+            ifdata->rx_length = 0;
+        }
 
-		/* Input */
-		uint8_t inputbyte = *buf++;
+        switch (ifdata->rx_mode) {
+            case KISS_MODE_NOT_STARTED:
 
-		/* If packet was too long */
-		if (ifdata->rx_length > ifdata->max_rx_length) {
-			//csp_log_warn("KISS RX overflow");
-			iface->rx_error++;
-			ifdata->rx_mode = KISS_MODE_NOT_STARTED;
-			ifdata->rx_length = 0;
-		}
+                /* Skip any characters until End char detected */
+                if (inputbyte != FEND) {
+                    break;
+                }
 
-		switch (ifdata->rx_mode) {
+                /* Try to allocate new buffer */
+                if (ifdata->rx_packet == NULL) {
+                    ifdata->rx_packet
+                        = pxTaskWoken ? csp_buffer_get_isr(0) : csp_buffer_get(0);  // CSP only supports one size
+                }
 
-		case KISS_MODE_NOT_STARTED:
+                /* If no more memory, skip frame */
+                if (ifdata->rx_packet == NULL) {
+                    ifdata->rx_mode = KISS_MODE_SKIP_FRAME;
+                    break;
+                }
 
-			/* Skip any characters until End char detected */
-			if (inputbyte != FEND) {
-				break;
-			}
+                /* Start transfer */
+                ifdata->rx_length = 0;
+                ifdata->rx_mode = KISS_MODE_STARTED;
+                ifdata->rx_first = true;
+                break;
 
-			/* Try to allocate new buffer */
-			if (ifdata->rx_packet == NULL) {
-				ifdata->rx_packet = pxTaskWoken ? csp_buffer_get_isr(0) : csp_buffer_get(0); // CSP only supports one size
-			}
+            case KISS_MODE_STARTED:
 
-			/* If no more memory, skip frame */
-			if (ifdata->rx_packet == NULL) {
-				ifdata->rx_mode = KISS_MODE_SKIP_FRAME;
-				break;
-			}
+                /* Escape char */
+                if (inputbyte == FESC) {
+                    ifdata->rx_mode = KISS_MODE_ESCAPED;
+                    break;
+                }
 
-			/* Start transfer */
-			ifdata->rx_length = 0;
-			ifdata->rx_mode = KISS_MODE_STARTED;
-			ifdata->rx_first = true;
-			break;
+                /* End Char */
+                if (inputbyte == FEND) {
+                    /* Accept message */
+                    if (ifdata->rx_length > 0) {
+                        /* Check for valid length */
+                        if (ifdata->rx_length < CSP_HEADER_LENGTH + sizeof(uint32_t)) {
+                            //csp_log_warn("KISS short frame skipped, len: %u", ifdata->rx_length);
+                            iface->rx_error++;
+                            ifdata->rx_mode = KISS_MODE_NOT_STARTED;
+                            break;
+                        }
 
-		case KISS_MODE_STARTED:
+                        /* Count received frame */
+                        iface->frame++;
 
-			/* Escape char */
-			if (inputbyte == FESC) {
-				ifdata->rx_mode = KISS_MODE_ESCAPED;
-				break;
-			}
+                        /* The CSP packet length is without the header */
+                        ifdata->rx_packet->length = ifdata->rx_length - CSP_HEADER_LENGTH;
 
-			/* End Char */
-			if (inputbyte == FEND) {
+                        /* Convert the packet from network to host order */
+                        ifdata->rx_packet->id.ext = csp_ntoh32(ifdata->rx_packet->id.ext);
 
-				/* Accept message */
-				if (ifdata->rx_length > 0) {
+                        /* Validate CRC */
+                        if (csp_crc32_verify(ifdata->rx_packet, false) != CSP_ERR_NONE) {
+                            //csp_log_warn("KISS invalid crc frame skipped, len: %u", ifdata->rx_packet->length);
+                            iface->rx_error++;
+                            ifdata->rx_mode = KISS_MODE_NOT_STARTED;
+                            break;
+                        }
 
-					/* Check for valid length */
-					if (ifdata->rx_length < CSP_HEADER_LENGTH + sizeof(uint32_t)) {
-						//csp_log_warn("KISS short frame skipped, len: %u", ifdata->rx_length);
-						iface->rx_error++;
-						ifdata->rx_mode = KISS_MODE_NOT_STARTED;
-						break;
-					}
+                        /* Send back into CSP, notice calling from task so last argument must be NULL! */
+                        csp_qfifo_write(ifdata->rx_packet, iface, pxTaskWoken);
+                        ifdata->rx_packet = NULL;
+                        ifdata->rx_mode = KISS_MODE_NOT_STARTED;
+                        break;
+                    }
 
-					/* Count received frame */
-					iface->frame++;
+                    /* Break after the end char */
+                    break;
+                }
 
-					/* The CSP packet length is without the header */
-					ifdata->rx_packet->length = ifdata->rx_length - CSP_HEADER_LENGTH;
+                /* Skip the first char after FEND which is TNC_DATA (0x00) */
+                if (ifdata->rx_first) {
+                    ifdata->rx_first = false;
+                    break;
+                }
 
-					/* Convert the packet from network to host order */
-					ifdata->rx_packet->id.ext = csp_ntoh32(ifdata->rx_packet->id.ext);
+                /* Valid data char */
+                ((char *)&ifdata->rx_packet->id.ext)[ifdata->rx_length++] = inputbyte;
 
-					/* Validate CRC */
-					if (csp_crc32_verify(ifdata->rx_packet, false) != CSP_ERR_NONE) {
-						//csp_log_warn("KISS invalid crc frame skipped, len: %u", ifdata->rx_packet->length);
-						iface->rx_error++;
-						ifdata->rx_mode = KISS_MODE_NOT_STARTED;
-						break;
-					}
+                break;
 
-					/* Send back into CSP, notice calling from task so last argument must be NULL! */
-					csp_qfifo_write(ifdata->rx_packet, iface, pxTaskWoken);
-					ifdata->rx_packet = NULL;
-					ifdata->rx_mode = KISS_MODE_NOT_STARTED;
-					break;
+            case KISS_MODE_ESCAPED:
 
-				}
+                /* Escaped escape char */
+                if (inputbyte == TFESC)
+                    ((char *)&ifdata->rx_packet->id.ext)[ifdata->rx_length++] = FESC;
 
-				/* Break after the end char */
-				break;
-			}
+                /* Escaped fend char */
+                if (inputbyte == TFEND)
+                    ((char *)&ifdata->rx_packet->id.ext)[ifdata->rx_length++] = FEND;
 
-			/* Skip the first char after FEND which is TNC_DATA (0x00) */
-			if (ifdata->rx_first) {
-				ifdata->rx_first = false;
-				break;
-			}
+                /* Go back to started mode */
+                ifdata->rx_mode = KISS_MODE_STARTED;
+                break;
 
-			/* Valid data char */
-			((char *) &ifdata->rx_packet->id.ext)[ifdata->rx_length++] = inputbyte;
+            case KISS_MODE_SKIP_FRAME:
 
-			break;
+                /* Just wait for end char */
+                if (inputbyte == FEND)
+                    ifdata->rx_mode = KISS_MODE_NOT_STARTED;
 
-		case KISS_MODE_ESCAPED:
-
-			/* Escaped escape char */
-			if (inputbyte == TFESC)
-				((char *) &ifdata->rx_packet->id.ext)[ifdata->rx_length++] = FESC;
-
-			/* Escaped fend char */
-			if (inputbyte == TFEND)
-				((char *) &ifdata->rx_packet->id.ext)[ifdata->rx_length++] = FEND;
-
-			/* Go back to started mode */
-			ifdata->rx_mode = KISS_MODE_STARTED;
-			break;
-
-		case KISS_MODE_SKIP_FRAME:
-
-			/* Just wait for end char */
-			if (inputbyte == FEND)
-				ifdata->rx_mode = KISS_MODE_NOT_STARTED;
-
-			break;
-
-		}
-
-	}
-
+                break;
+        }
+    }
 }
 
-int csp_kiss_add_interface(csp_iface_t * iface) {
+int csp_kiss_add_interface(csp_iface_t *iface) {
+    if ((iface == NULL) || (iface->name == NULL) || (iface->interface_data == NULL)) {
+        return CSP_ERR_INVAL;
+    }
 
-	if ((iface == NULL) || (iface->name == NULL) || (iface->interface_data == NULL)) {
-		return CSP_ERR_INVAL;
-	}
+    csp_kiss_interface_data_t *ifdata = iface->interface_data;
+    if (ifdata->tx_func == NULL) {
+        return CSP_ERR_INVAL;
+    }
 
-        csp_kiss_interface_data_t * ifdata = iface->interface_data;
-	if (ifdata->tx_func == NULL) {
-		return CSP_ERR_INVAL;
-	}
+    if (csp_mutex_create(&ifdata->lock) != CSP_MUTEX_OK) {
+        return CSP_ERR_NOMEM;
+    }
 
-	if (csp_mutex_create(&ifdata->lock) != CSP_MUTEX_OK) {
-		return CSP_ERR_NOMEM;
-        }
+    ifdata->max_rx_length = CSP_HEADER_LENGTH + csp_buffer_data_size();  // CSP header + CSP data
+    ifdata->rx_length = 0;
+    ifdata->rx_mode = KISS_MODE_NOT_STARTED;
+    ifdata->rx_first = false;
+    ifdata->rx_packet = NULL;
 
-	ifdata->max_rx_length = CSP_HEADER_LENGTH + csp_buffer_data_size(); // CSP header + CSP data
-	ifdata->rx_length = 0;
-	ifdata->rx_mode = KISS_MODE_NOT_STARTED;
-	ifdata->rx_first = false;
-	ifdata->rx_packet = NULL;
+    const unsigned int max_data_size = csp_buffer_data_size() - sizeof(uint32_t);  // compensate for the added CRC32
+    if ((iface->mtu == 0) || (iface->mtu > max_data_size)) {
+        iface->mtu = max_data_size;
+    }
 
-        const unsigned int max_data_size = csp_buffer_data_size() - sizeof(uint32_t); // compensate for the added CRC32
-        if ((iface->mtu == 0) || (iface->mtu > max_data_size)) {
-            iface->mtu = max_data_size;
-        }
+    iface->nexthop = csp_kiss_tx;
 
-	iface->nexthop = csp_kiss_tx;
-
-	return csp_iflist_add(iface);
+    return csp_iflist_add(iface);
 }
--- src/bss_lib/uart_csp/uart_csp.c
+++ src/bss_lib/uart_csp/uart_csp.c
@@ -6,9 +6,12 @@
 #include "led/led.h"
 #include "uart_csp/uart_csp.h"
 
-#define UART_BUFFER_SIZE 64
-#define TASK_RX_MONITOR_STACK_SIZE (2048 / sizeof(portSTACK_TYPE))
-#define TASK_RX_MONITOR_STACK_PRIORITY (tskIDLE_PRIORITY)
+#define UART_BUFFER_SIZE 512
+#define PACKET_RX_PERIOD_MS (5 / portTICK_PERIOD_MS)
+#define TASK_RX_MONITOR_STACK_SIZE (512 / sizeof(portSTACK_TYPE))
+#define TASK_RX_MONITOR_STACK_PRIORITY (1 + tskIDLE_PRIORITY)
+#define TASK_CSP_SERVER_STACK_SIZE (2048 / sizeof(portSTACK_TYPE))
+#define TASK_CSP_SERVER_STACK_PRIORITY (2 + tskIDLE_PRIORITY)
 
 volatile uint8_t uart_rx_buffer[UART_BUFFER_SIZE];
 volatile uint32_t uart_rx_head = 0;
@@ -17,6 +20,10 @@ volatile uint32_t uart_rx_tail = 0;
 const char *kiss_device = NULL;
 csp_usart_callback_t uart_csp_rx_callback = NULL;
 void *uart_csp_user_data = NULL;
+
+/***********************************************************
+ * Config Steps
+ ***********************************************************/
 
 static void uart_csp_hw_configure(void) {
     /* Enable the Comms UART peripheral clock */
@@ -45,64 +52,6 @@ static void uart_csp_hw_configure(void) {
         /* Couldn't setup UART */
         /* TODO: Add assert */
     }
-}
-
-int csp_usart_open(
-    const csp_usart_conf_t *conf, csp_usart_callback_t rx_callback, void *user_data, csp_usart_fd_t *return_fd
-) {
-    /* Hardware should get configured based on data passed in via conf however
-     * for brevity the UART HW configuration is hardcoded via uart_csp_hw_configure
-     * TODO: Update uart_csp_hw_configure to use the settings in conf
-     */
-    uart_csp_hw_configure();
-    uart_csp_rx_callback = rx_callback;
-    uart_csp_user_data = user_data;
-    return CSP_ERR_NONE;
-}
-
-int csp_usart_write(csp_usart_fd_t fd, const void *data, size_t data_length) {
-    size_t i;
-    const uint8_t *byte_data = (const uint8_t *)data;
-
-    for (i = 0; i < data_length; i++) {
-        uart_write(UART_CSP_PORT, byte_data[i]);
-    }
-    return CSP_ERR_TX;  // best matching CSP error code.
-}
-
-static void uart_csp_buffer_push(uint8_t value) {
-    uint32_t next_head = (uart_rx_head + 1) % UART_BUFFER_SIZE;
-
-    if (next_head != uart_rx_tail) {
-        uart_rx_buffer[uart_rx_head] = value;
-        uart_rx_head = next_head;
-    } else {
-        /* TODO: Handle buffer overflow error */
-    }
-}
-
-static bool uart_csp_buffer_pop(uint8_t *value) {
-    bool popped = false;
-
-    if (uart_rx_tail != uart_rx_head) {
-        *value = uart_rx_buffer[uart_rx_tail];
-        uart_rx_tail = (uart_rx_tail + 1) % UART_BUFFER_SIZE;
-        popped = true;
-    }
-
-    return popped;
-}
-
-static uint8_t uart_csp_buffer_len() {
-    uint8_t diff = 0;
-
-    if (uart_rx_tail > uart_rx_head) {
-        diff = uart_rx_tail - uart_rx_head;
-    } else {
-        diff = uart_rx_head - uart_rx_tail;
-    }
-
-    return diff;
 }
 
 static void uart_csp_lib_configure() {
@@ -134,17 +83,147 @@ static void uart_csp_lib_configure() {
         csp_log_error("failed to add KISS interface [%s], error: %d", kiss_device, error);
         exit(1);
     }
+
+    csp_rtable_set(CSP_DEFAULT_ROUTE, 0, default_iface, CSP_NO_VIA_ADDRESS);
 }
 
-static void task_rx(void *pvParameters) {
+/***********************************************************
+ * Buffer
+ ***********************************************************/
+
+static void uart_csp_buffer_push(uint8_t value) {
+    uint32_t next_head = (uart_rx_head + 1) % UART_BUFFER_SIZE;
+
+    if (next_head != uart_rx_tail) {
+        uart_rx_buffer[uart_rx_head] = value;
+        uart_rx_head = next_head;
+    } else {
+        /* TODO: Handle buffer overflow error */
+        csp_log_error("CSP UART Rx Buffer Full");
+        exit(1);
+    }
+}
+
+static bool uart_csp_buffer_pop(uint8_t *value) {
+    bool popped = false;
+
+    if (uart_rx_tail != uart_rx_head) {
+        *value = uart_rx_buffer[uart_rx_tail];
+        uart_rx_tail = (uart_rx_tail + 1) % UART_BUFFER_SIZE;
+        popped = true;
+    }
+
+    return popped;
+}
+
+static uint8_t uart_csp_buffer_len() {
+    uint8_t diff = 0;
+
+    if (uart_rx_tail > uart_rx_head) {
+        diff = uart_rx_tail - uart_rx_head;
+    } else {
+        diff = uart_rx_head - uart_rx_tail;
+    }
+
+    return diff;
+}
+
+/***********************************************************
+ * CSP Callbacks
+ ***********************************************************/
+
+int csp_usart_open(
+    const csp_usart_conf_t *conf, csp_usart_callback_t rx_callback, void *user_data, csp_usart_fd_t *return_fd
+) {
+    /* Hardware should get configured based on data passed in via conf however
+     * for brevity the UART HW configuration is hardcoded via uart_csp_hw_configure
+     * TODO: Update uart_csp_hw_configure to use the settings in conf
+     */
+    uart_csp_hw_configure();
+    uart_csp_rx_callback = rx_callback;
+    uart_csp_user_data = user_data;
+    return CSP_ERR_NONE;
+}
+
+int csp_usart_write(csp_usart_fd_t fd, const void *data, size_t data_length) {
+    size_t i;
+    int return_val = 0;
+    const uint8_t *byte_data = (const uint8_t *)data;
+
+    for (i = 0; i < data_length; i++) {
+        while (!uart_is_tx_ready(UART_CSP_PORT)) {
+            /* NOP: Wait till the transmitter is clear */
+        }
+
+        if (!uart_write(UART_CSP_PORT, byte_data[i])) {
+            /* write success */
+            return_val++; /* record how many bytes get written */
+        } else {
+            /* write failure */
+            return_val = CSP_ERR_TX;
+            break;
+        }
+    }
+
+    return return_val;
+}
+
+/***********************************************************
+ * Tasks
+ ***********************************************************/
+
+static void uart_csp_task_rx(void *pvParameters) {
     uint8_t rx_byte = 0;
     while (1) {
-        if (uart_csp_buffer_len()) {
+        while (uart_csp_buffer_len()) {
             if (uart_csp_buffer_pop(&rx_byte)) {
                 uart_csp_rx_callback(uart_csp_user_data, &rx_byte, 1, NULL);
             }
         }
-        vTaskDelay(100);
+        vTaskDelay(PACKET_RX_PERIOD_MS);
+    }
+}
+
+static void uart_csp_task_server(void *pvParameters) {
+    csp_log_info("Server task started");
+
+    /* Create socket with no specific socket options, e.g. accepts CRC32, HMAC, XTEA, etc. if enabled during compilation */
+    csp_socket_t *sock = csp_socket(CSP_SO_NONE);
+
+    /* Bind socket to all ports, e.g. all incoming connections will be handled here */
+    csp_bind(sock, CSP_ANY);
+
+    /* Create a backlog of 10 connections, i.e. up to 10 new connections can be queued */
+    csp_listen(sock, 10);
+
+    /* Wait for connections and then process packets on the connection */
+    while (1) {
+        /* Wait for a new connection, 10000 mS timeout */
+        csp_conn_t *conn;
+        if ((conn = csp_accept(sock, 10000)) == NULL) {
+            /* timeout */
+            continue;
+        }
+
+        /* Read packets on connection, timout is 100 mS */
+        csp_packet_t *packet;
+        while ((packet = csp_read(conn, 50)) != NULL) {
+            switch (csp_conn_dport(conn)) {
+                case 0x21:
+                    /* Process packet here */
+                    csp_log_info("Packet received on MY_SERVER_PORT: %s", (char *)packet->data);
+                    csp_buffer_free(packet);
+                    break;
+
+                default:
+                    /* Call the default CSP service handler, handle pings, buffer use, etc. */
+                    csp_service_handler(conn, packet);
+                    break;
+            }
+        }
+
+        /* Close current connection */
+        csp_close(conn);
     }
 }
 
@@ -158,11 +237,25 @@ void uart_csp_init(void) {
     uart_csp_lib_configure();
 
     csp_log_info("Launching RX Monitor task");
-    if (xTaskCreate(task_rx, "RX Monitor", TASK_RX_MONITOR_STACK_SIZE, NULL, TASK_RX_MONITOR_STACK_PRIORITY, NULL)
+    if (xTaskCreate(
+            uart_csp_task_rx, "RX Monitor", TASK_RX_MONITOR_STACK_SIZE, NULL, TASK_RX_MONITOR_STACK_PRIORITY, NULL
+        )
         != pdPASS) {
         printf("Failed to create RX Monitor task\r\n");
     }
+
+    csp_log_info("Launching CSP Server task");
+    if (xTaskCreate(
+            uart_csp_task_server, "CSP Server", TASK_CSP_SERVER_STACK_SIZE, NULL, TASK_CSP_SERVER_STACK_PRIORITY, NULL
+        )
+        != pdPASS) {
+        printf("Failed to create CSP Server task\r\n");
+    }
 }
+
+/***********************************************************
+ * Interrupts
+ ***********************************************************/
 
 void uart_csp_irq_handler(void) {
     uint32_t status = uart_get_status(UART_CSP_PORT);
