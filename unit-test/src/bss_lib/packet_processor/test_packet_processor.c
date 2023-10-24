1973ba60e6cc5694b0b349cfa8f5a73eafd678bf
Colin Waddell <cwaddell@benchmark-space.com>
2023-10-24 14:59:27 +0100

    testing response to empty message

--- unit-test/src/bss_lib/packet_processor/test_packet_processor.c
+++ unit-test/src/bss_lib/packet_processor/test_packet_processor.c
@@ -8,6 +8,8 @@
 #include "packet_processor/packet_utilities.h"
 #include "status.h"
 
+#define GET_PACKET_STATUS(buf) ((status_t)((buf[1] << 8) | buf[0]))
+
 void setUp(void) {
     /* Per test setup */
 }
@@ -16,13 +18,13 @@ void tearDown(void) {
     /* Per test cleanup */
 }
 
-void test_basic_message() {
+void test_EmptyMessage() {
     uint8_t packet[] = {};
-    uint16_t packet_len = sizeof(packet);
     uint8_t tx_buffer[PACKET_BUFFER_SIZE];
     uint8_t tx_len = 0;
 
-    tx_len = process_packet(packet, packet_len, tx_buffer);
+    tx_len = process_packet(packet, sizeof(packet), tx_buffer);
 
-    TEST_ASSERT_EQUAL(tx_len, 2);
+    TEST_ASSERT_EQUAL(2, tx_len); /* Only contains error message (16 bits) */
+    TEST_ASSERT_EQUAL(STATUS_COMMS_INCORRECT_INPUT_DATA, GET_PACKET_STATUS(tx_buffer));
 }
