c6902087d4810cfc6c11efa49af461d9f37e7466
Colin Waddell <cwaddell@benchmark-space.com>
2023-10-26 11:54:03 +0100

    splitting up more code

---
+++ src/lib/hooks/hooks.c
@@ -0,0 +1,37 @@
+#include "hooks.h"
+#include "FreeRTOS.h"
+#include "task.h"
+#include <stdio.h>
+
+extern void vApplicationStackOverflowHook(TaskHandle_t xTask, char *pcTaskName);
+extern void vApplicationTickHook(void);
+extern void vApplicationMallocFailedHook(void);
+extern void xPortSysTickHandler(void);
+
+/**
+ * \brief Called if stack overflow during execution
+ */
+extern void vApplicationStackOverflowHook(TaskHandle_t xTask, char *pcTaskName) {
+    printf("stack overflow %x %s\r\n", &xTask, (portCHAR *)pcTaskName);
+    /* If the parameters have been corrupted then inspect pxCurrentTCB to
+	 * identify which task has overflowed its stack.
+	 */
+    for (;;) { }
+}
+
+/**
+ * \brief This function is called by FreeRTOS each tick
+ */
+extern void vApplicationTickHook(void) {
+}
+
+extern void vApplicationMallocFailedHook(void) {
+    /* Called if a call to pvPortMalloc() fails because there is insufficient
+	free memory available in the FreeRTOS heap.  pvPortMalloc() is called
+	internally by FreeRTOS API functions that create tasks, queues, software
+	timers, and semaphores.  The size of the FreeRTOS heap is set by the
+	configTOTAL_HEAP_SIZE configuration constant in FreeRTOSConfig.h. */
+
+    /* Force an assert. */
+    configASSERT((volatile void *)NULL);
+}
